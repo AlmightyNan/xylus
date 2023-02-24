72025647e94363db697d092b26a62a23ecb9c820
AlmightyNan <93263738+AlmightyNan@users.noreply.github.com>
2023-02-24 19:09:28 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,173 @@
+import bot from "../assets/bot.svg";
+import user from "../assets/user.svg";
+
+const form = document.querySelector(".chatform");
+const chatContainer = document.getElementById("chatContainer");
+// const temperatureSlider = document.getElementById("temperature");
+const instagram = document.getElementById("instagram");
+const twitter = document.getElementById("twitter");
+const promptList = document.getElementById("promptList");
+// const model = document.getElementById("model");
+const overlay = document.getElementById("overlayContent");
+const save = document.querySelectorAll("#save");
+let responses = [];
+let loadInterval;
+
+instagram.addEventListener("click", () => {
+  window.open('https://instagram.com/a1mightynan');
+});
+
+twitter.addEventListener("click", () => {
+  window.open('https://twitter.com/almightynan')
+})
+function loader(element) {
+  element.textContent = "";
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+    if (element.textContent.length > 3) {
+      element.textContent = "";
+    }
+  }, 400);
+}
+
+async function addToPromptList(id) {
+  const responseToSave = responses.find(response => response.id === id);
+  const response = await fetch(`http://localhost:3000/prompt/savePrompt`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ response: responseToSave.response, prompt: responseToSave.prompt}),
+  });
+  if (response.status === 200) {
+    promptList.innerHTML += `<li>Q: ${responseToSave.prompt}<br> A: ${responseToSave.response}</li>`;
+  }
+}
+
+(async function retrievePrompts() {
+  const response = await fetch(`http://localhost:3000/prompt/savedPrompts`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.status === 200) {
+    const json = await response.json();
+    json.forEach((saved) => {
+      promptList.innerHTML += `<li>Q: ${saved.prompt}<br> A: ${saved.response}</li>`;
+    });
+  }
+})();
+
+// deleteAll.addEventListener("click", async () => {
+//   const response = await fetch(`http://localhost:3000/prompt/deleteAll`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+//   if (response.status === 200) {
+//     promptList.innerHTML = "";
+//   }
+// });
+
+document.addEventListener("click", saveListener);
+function saveListener(e) {
+  if (e.target && e.target.id == "save") {
+    const parentWrapper = e.target.closest('.wrapper');
+    const id = parentWrapper.querySelector('.message').id;
+    addToPromptList(id);
+  }
+}
+
+function typeText(element, text) {
+  let i = 0;
+  let typeInterval = setInterval(() => {
+    if (i < text.length) {
+      element.innerHTML += text.charAt(i);
+      i++;
+    } else {
+      clearInterval(typeInterval);
+    }
+  }, 20);
+}
+
+function getUniqueId() {
+  const time = Date.now();
+  const randomNum = Math.floor(Math.random() * 100000000);
+  return `id-${time}-${randomNum}`;
+}
+
+function chatBubble(isAi, value, uniqueId) {
+  return `
+        <div class="wrapper ${isAi && "ai"}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? "bot" : "user"}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+                ${
+                  isAi
+                    ? ``
+                    : ""
+                }
+            </div>
+        </div>
+            `;
+}
+
+const handleFormSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  chatContainer.innerHTML += chatBubble(false, data.get("prompt"));
+  form.reset();
+  const uniqueId = getUniqueId();
+  chatContainer.innerHTML += chatBubble(true, " ", uniqueId);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  const messageDiv = document.getElementById(uniqueId);
+  const temperature = 0.95;
+  const prompt = data.get("prompt");
+  
+
+  loader(messageDiv);
+
+  const response = await fetch("http://localhost:3000/chatbot", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: 'text-davinci-003',
+      prompt: data.get("prompt"),
+      temperature: 0.95,
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = " ";
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim()
+    typeText(messageDiv, parsedData);
+    console.log(data);
+    responses.push({
+      prompt: prompt,
+      response: parsedData,
+      id: uniqueId
+    });
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
+};
+
+form.addEventListener("submit", handleFormSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleFormSubmit(e);
+  }
+});
---
+++ js/sidemenu.js
@@ -0,0 +1,23 @@
+// const temperature = document.getElementById("temperature");
+// const output = document.getElementById("output");
+// output.innerHTML = temperature.value;
+
+// temperature.oninput = function () {
+//   output.innerHTML = this.value;
+// };
+// const list = document.getElementById("model");
+
+// const models = [
+//   "text-davinci-003",
+//   "code-davinci-002",
+//   "text-curie-001"
+// ];
+
+
+// for (let i = 0; i < models.length; i++) {
+//   let option = models[i];
+//   const el = document.createElement("option");
+//   el.textContent = option;
+//   el.value = option;
+//   list.appendChild(el);
+// }
